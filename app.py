01159315bdfdde5c243202b5597bbb7441765fa0
Enrique27-L <libreheinrich051@gmail.com>
2025-08-17 20:57:14 -0500

    Semana-9

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return '¡Hola! Bienvenido a mi aplicación.'
+    return '¡Hola! Bienvenido a mi aplicación'
 
 
 @app.route('/usuario/<nombre>')
