959c20f8d5b36aeb7de09fb4358640644f6eef54
Enrique27-L <libreheinrich051@gmail.com>
2025-08-17 20:56:53 -0500

    Primer commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return '¡Hola! Bienvenido a mi aplicación Flask.'
+    return '¡Hola! Bienvenido a mi aplicación.'
 
 
 @app.route('/usuario/<nombre>')
